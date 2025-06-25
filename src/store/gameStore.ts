385fecdbd007afaa90d3ce54b970e99d216720b5
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-06-25 12:24:28 +0500

    Refactor game logic: extract winning combinations to a constants file, delete types/game.ts, and update game store structure with combine middleware to automatically infer types and create state + action together

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -1,16 +1,9 @@
 import { create } from "zustand";
-import { type Player, type Cell, type GameTypes } from "../types/game";
+import { combine } from "zustand/middleware";
+import { winningCombos } from "../constants/winningCombinations";
 
-const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+type Player = "X" | "O";
+type Cell = Player | null;
 
 // Winner finder function
 function checkWinner(board: Cell[]): Player | "Draw" | null {
@@ -22,35 +15,43 @@ function checkWinner(board: Cell[]): Player | "Draw" | null {
   return board.every((n) => n !== null) ? "Draw" : null;
 }
 // To create the game store
-export const useGameStore = create<GameTypes>((set, get) => ({
-  board: Array(9).fill(null),
-  currentPlayer: "X",
-  winner: null,
-
-  // Who is next?
-  playTurn: (index: number) => {
-    const { board, currentPlayer, winner } = get();
-
-    // if the clicked square is already clicked or winner is known, ignore return nothing;
-    if (board[index] || winner) return null;
-
-    // copy the board, give "X" or "O" respectively
-    const newBoard = [...board];
-    newBoard[index] = currentPlayer;
-
-    // Call winner finder function
-    const newWinner = checkWinner(newBoard);
-
-    set({
-      board: newBoard,
-      currentPlayer: currentPlayer === "X" ? "O" : "X",
-      winner: newWinner,
-    });
-  },
-  resetGame: () =>
-    set({
+export const useGameStore = create(
+  combine(
+    // Initial values
+    {
       board: Array(9).fill(null),
       currentPlayer: "X",
-      winner: null,
-    }),
-}));
+      winner: null as Player | "Draw" | null,
+    },
+    // Actions
+    (set, get) => ({
+      // Next player's turn
+      playTurn: (index: number) => {
+        const { board, currentPlayer, winner } = get();
+
+        // if the clicked square is already clicked or winner is known, ignore return nothing;
+        if (board[index] || winner) return null;
+
+        // copy the board, give "X" or "O" respectively
+        const newBoard = [...board];
+        newBoard[index] = currentPlayer;
+
+        // Call winner finder function
+        const newWinner = checkWinner(newBoard);
+
+        set({
+          board: newBoard,
+          currentPlayer: currentPlayer === "X" ? "O" : "X",
+          winner: newWinner,
+        });
+      },
+      // Restart the game
+      resetGame: () =>
+        set({
+          board: Array(9).fill(null),
+          currentPlayer: "X",
+          winner: null,
+        }),
+    })
+  )
+);
